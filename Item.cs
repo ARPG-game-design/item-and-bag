d0ca25aaff741b6d73bd46c37b5b05a024bea8ca
nn0629 <50348768+nn0629@users.noreply.github.com>
2021-03-17 23:16:53 +0800

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName="New Item",menuName="New Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemImage;
+    public int itemHeld;
+
+    [TextArea]
+    public string itemInfo;
+}
