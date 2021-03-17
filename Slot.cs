83eda38e3fcdb55d9815310567d7dddddaaced86
nn0629 <50348768+nn0629@users.noreply.github.com>
2021-03-17 23:15:41 +0800

    Add files via upload

---
+++ Slot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public int slotID; //背包裡格子物品ID
+    public Item slotItem;
+    public Image slotImage; //物品圖
+    public Text slotNum; //物品數量顯示
+    public string slotInfo;
+
+    public GameObject itemInSlot;
+
+    public void ItemOnClicked()
+    {
+        BagManager.UpdateItemInfo(slotInfo);
+    }
+
+    public void SetupSlot(Item item)
+    {
+        if(item == null)
+        {
+            itemInSlot.SetActive(false);
+            return;
+        }
+
+        slotImage.sprite = item.itemImage;
+        slotNum.text = item.itemHeld.ToString();
+        slotInfo = item.itemInfo;
+    }
+}
